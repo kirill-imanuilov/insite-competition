7844dcdb8b8cfe054c6121fd08fbc6a6daecf9de
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-12-02 18:50:12 +0300

    feat: add an about block
    - Add an about block
    - Add a game plot to the about block

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,11 +1,13 @@
 import './index.css';
 import { Title } from './components/blocks/Title';
+import { About } from './components/blocks/About';
 
 export function Page() {
   return (
     <div className='page'>
       <div className='page-content'>
         <Title />
+        <About />
       </div>
     </div>
   );
